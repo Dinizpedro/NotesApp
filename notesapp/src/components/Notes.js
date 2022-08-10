2cdcb1fb37972ebb0769aa939b2a984382f042f4
pedro <1170053@isep.ipp.pt>
2022-08-10 15:40:03 +0100

    Show all notes

--- notesapp/src/components/Notes.js
+++ notesapp/src/components/Notes.js
@@ -2,15 +2,26 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import {Container, Paper} from "@mui/material";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import Button from "@mui/material/Button";
 
 export default function Notes() {
-    const paperStyle = {padding:'50px 20px', width: 600, margin:"20px auto"}
+    const paperStyle = {padding:'20px 20px', width: 600, margin:"20px auto"}
     const[name,setName] = useState('')
     let result = ''
     const [mess, setMess] = React.useState('');
     const[description,setDescription] = useState('')
+    const[allnotes,setAllnotes]= useState( [])
+
+    useEffect(()=>{
+        fetch("http://localhost:8080/notes")
+            .then(res=>res.json())
+            .then((result)=>{
+                setAllnotes(result);
+                }
+            )
+    },[])
+
     const handleClick =(e)=> {
         e.preventDefault()
         const notes = {name,description}
@@ -31,7 +42,7 @@ export default function Notes() {
     return (
         <Container>
         <Paper elevation={3} style ={paperStyle}>
-            <h1 style={{color:"blue"}}><u>Add a note</u></h1>
+            <h1> Add a note </h1>
         <Box
             component="form"
             sx={{
@@ -46,11 +57,27 @@ export default function Notes() {
             <Button variant="contained" color="secondary" onClick={handleClick}>
                 Submit
             </Button>
-
-        </Box>
             <h1></h1>
             {result}
+        </Box>
+
         </Paper>
+
+            <h1> Notes </h1>
+            <Paper elevation={3} style={paperStyle}>
+
+                {
+                    allnotes.map(singleNote=>(
+                        <Paper elevation={6} style={{margin:"10px",padding:"15px", textAlign:"left"}} key={singleNote.id}>
+                            Date: {singleNote.creationDate}<br/>
+                            Name:{singleNote.name}<br/>
+                            Address:{singleNote.description}
+
+                        </Paper>
+                    ))
+                }
+            </Paper>
+
         </Container>
     );
 }
